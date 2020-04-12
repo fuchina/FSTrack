856e3be873462730d1d43a671e24dc3cdf727716
fuchina <1245102331@qq.com>
2020-04-12 22:42:48 +0800

    全部统计

--- FSTrack/Classes/FSTrack.m
+++ FSTrack/Classes/FSTrack.m
@@ -12,9 +12,9 @@
 @implementation FSTrack
 
 + (void)configUMeng:(NSString *)appKey channelId:(NSString *)channelId crashReportEnabled:(BOOL)crashReportEnabled appVersion:(NSString *)appVersion PUID:(NSString *)puid{
-#ifdef __OPTIMIZE__
-#if TARGET_IPHONE_SIMULATOR
-#else
+//#ifdef __OPTIMIZE__
+//#if TARGET_IPHONE_SIMULATOR
+//#else
     UMConfigInstance.appKey = appKey;
     UMConfigInstance.channelId = channelId;
     [MobClick startWithConfigure:UMConfigInstance];//配置以上参数后调用此方法初始化SDK！
@@ -22,36 +22,36 @@
     [MobClick setCrashReportEnabled:crashReportEnabled];
     [MobClick setAppVersion:appVersion];
     [MobClick profileSignInWithPUID:puid];
-#endif
-#endif
+//#endif
+//#endif
 }
 
 // Release模式下才统计
 + (void)event:(NSString *)event{
-#ifdef __OPTIMIZE__
-#if TARGET_IPHONE_SIMULATOR
-#else
+//#ifdef __OPTIMIZE__
+//#if TARGET_IPHONE_SIMULATOR
+//#else
     [MobClick event:event];
-#endif
-#endif
+//#endif
+//#endif
 }
 
 + (void)pageStart:(NSString *)page{
-#ifdef __OPTIMIZE__
-#if TARGET_IPHONE_SIMULATOR
-#else
+//#ifdef __OPTIMIZE__
+//#if TARGET_IPHONE_SIMULATOR
+//#else
     [MobClick beginLogPageView:page];
-#endif
-#endif
+//#endif
+//#endif
 }
 
 + (void)pageEnd:(NSString *)page{
-#ifdef __OPTIMIZE__
-#if TARGET_IPHONE_SIMULATOR
-#else
+//#ifdef __OPTIMIZE__
+//#if TARGET_IPHONE_SIMULATOR
+//#else
     [MobClick endLogPageView:page];
-#endif
-#endif
+//#endif
+//#endif
 }
 
 @end
